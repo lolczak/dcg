e1bc870757122b94a22e71128d609fe38cae265d
lolczak <lukasz.jan.olczak@gmail.com>
2015-08-21 15:26:27 +0200

    small changes

--- src/DCG/ChartParser.hs
+++ src/DCG/ChartParser.hs
@@ -1,2 +1,27 @@
 module DCG.ChartParser where
 
+import DCG.Grammar
+import qualified Data.Set as Set
+
+data Edge = Passive {start :: Int, end :: Int, rule :: Production}
+          | Active  {start :: Int, end :: Int, rule :: Production, remaining :: [Production]}
+
+
+type State = Set.Set Edge
+
+type Chart = [State]
+
+buildChart :: [String] -> Grammar -> [Edge]
+buildChart = fail "sdfsdf"
+
+scan :: [String] -> Lexicon -> Chart
+scan ts l =
+    map findTerminal ts
+    where findTerminal w = case findInLexicon l w of
+                                Just xs -> 
+                                Nothing -> Set.empty
+
+
+--predict
+
+--combine
--- src/DCG/Grammar.hs
+++ src/DCG/Grammar.hs
@@ -1,5 +1,7 @@
 module DCG.Grammar where
 
+import qualified Data.Map as Map
+
 data Grammar = Grammar {start :: String, productions :: [Production]}
 
 data Production = Production Lhs Rhs
@@ -8,7 +10,15 @@ data Lhs = Lhs String
 
 data Rhs = Seq [Rhs]
          | Alt Rhs Rhs
-         | Lexem String
+
+data LexProduction = LexProduction Lhs LexRhs
+
+type LexRhs = [String]
+
+type Lexicon = Map.Map String [Lhs]
+
+findInLexicon :: Lexicon -> String -> Maybe [Lhs]
+findInLexicon l w = Map.lookup w l
 
 infix 9 ~>
 (~>) :: Lhs -> Rhs -> Production
