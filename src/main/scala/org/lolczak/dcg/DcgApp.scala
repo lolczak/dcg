53d43b6759c9395808dbbfb35c5b07b695978263
lolczak <lukasz.jan.olczak@gmail.com>
2015-09-05 17:58:17 +0200

    App

--- src/main/scala/org/lolczak/dcg/DcgApp.scala
+++ src/main/scala/org/lolczak/dcg/DcgApp.scala
@@ -1,5 +1,52 @@
 package org.lolczak.dcg
 
+import java.io.File
+
+import org.lolczak.dcg.parser.grammar.GrammarParser
+import org.lolczak.dcg.parser.language.{Leaf, Node, ChartParser, ParseTree}
+
+import scala.io.Source
+
 object DcgApp extends App {
+
+  val grammarPath = args(0)
+
+  if (args.length != 2) {
+    printHelp()
+    System.exit(1)
+  }
+  val utterance = args(1)
+  val (lexicon, grammar) = GrammarParser.parseGrammar(Source.fromFile(new File(grammarPath), "UTF-8").mkString).get
+  val results = ChartParser.parseDcg(grammar, lexicon, utterance)
+
+  def printHelp() = {
+    println("Usage: dcg <grammar_path> <utterance>")
+  }
+
+  render(results)
+
+  def render(results: List[ParseTree[Term, String]]): Unit = {
+    println(s"Found ${results.size} results.")
+    (results zip Stream.from(0)).foreach { case (r, idx) =>
+      println(s"Result no $idx:")
+      render(r)
+    }
+  }
+
+  def render(root: ParseTree[Term, String]): Unit = {
+    def renderNode(node: ParseTree[Term, String], depth:Int=0): Unit = {
+      node match {
+        case Leaf(symbol) => printIdention(depth); println(s"Leaf '$symbol'")
+        case Node(term, children) => printIdention(depth); println(s"Node ${term.name}"); children.foreach(renderNode(_, depth+1))
+      }
+    }
+    renderNode(root, 0)
+  }
+
+  def printIdention(depth: Int): Unit = {
+    if (depth <= 0) ()
+    else (1 to depth) foreach (_ => print("  "))
+  }
+
 
 }
--- src/main/scala/org/lolczak/dcg/parser/language/binding/VarAssignments.scala
+++ src/main/scala/org/lolczak/dcg/parser/language/binding/VarAssignments.scala
@@ -7,23 +7,27 @@ import scalaz.{-\/, \/, \/-}
 
 case class VarAssignments(assignments: Map[String, FValue]) {
 
-  //  def +(varName: String, value: FValue): VarAssignments = ???
-
+  /**
+   * Combines variable assignments.
+   *
+   * @param that
+   * @return
+   */
   def combine(that: VarAssignments): VarAssignments.Error \/ VarAssignments = {
     val varNames: Set[String] = this.assignments.keySet ++ that.assignments.keySet
     val tuples: Set[(String, Option[FValue], Option[FValue])] = for {
       varName <- varNames
-      left  = this.assignments.get(varName)
+      left = this.assignments.get(varName)
       right = that.assignments.get(varName)
     } yield (varName, left, right)
     val inconsistents = tuples.find {
       case (varName, left, right) =>
-        val diff = for (l<-left; r<-right) yield l!=r
+        val diff = for (l <- left; r <- right) yield l != r
         diff.getOrElse(false)
     } map (_._1)
     if (inconsistents.nonEmpty) -\/(s"Inconsistency for $inconsistents")
     else {
-      val pairs: Set[(String, FValue)] = tuples.map(x=>(x._1, x._2.orElse(x._3).get))
+      val pairs: Set[(String, FValue)] = tuples.map(x => (x._1, x._2.orElse(x._3).get))
       val map: Map[String, FValue] = pairs.foldLeft(Map.empty[String, FValue]) {
         case (acc, pair) => acc.+(pair)
       }
@@ -77,7 +81,7 @@ object VarAssignments {
   }
 
   /**
-   * Creates variable assignments.
+   * Creates variable assignments based on rule features and features values derived from parsed nodes.
    *
    * @param ruleFeatures
    * @param parsedFeatures
@@ -87,14 +91,20 @@ object VarAssignments {
     val varNames = ruleFeatures.features.filter(_._2.isVariable) map { case (featName, FVariable(varName)) => (varName, featName) }
     val assignments: Map[String, FValue] = for {
       (varName, featName) <- varNames
-      if parsedFeatures.features.contains(featName) && !parsedFeatures.features(featName).isVariable//todo encapsulate it
+      if parsedFeatures.features.contains(featName) && !parsedFeatures.features(featName).isVariable //todo encapsulate it
     } yield (varName, parsedFeatures.features(featName))
 
     VarAssignments(assignments)
   }
 
+  /**
+   * Binds all variables in lhs of production.
+   *
+   * @param termFeatures
+   * @param assignments
+   * @return
+   */
   def bind(termFeatures: FeatureStruct, assignments: VarAssignments): Error \/ FeatureStruct = {
-
     val map = termFeatures.features.foldLeft[Error \/ Map[String, FValue]](\/-(Map.empty)) {
       case (result@ -\/(_), _) => result
       case (\/-(feats), (feature, FVariable(varName))) =>
