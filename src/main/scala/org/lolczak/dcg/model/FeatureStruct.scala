b3a9f7821067103934e6f2558413c12ea4517c50
lolczak <lukasz.jan.olczak@gmail.com>
2015-10-08 14:17:00 +0200

    refactoring

--- src/main/scala/org/lolczak/dcg/model/FeatureStruct.scala
+++ src/main/scala/org/lolczak/dcg/model/FeatureStruct.scala
@@ -2,15 +2,11 @@ package org.lolczak.dcg.model
 
 import org.lolczak.dcg.parser.language.variable.VariableAssignment
 
-sealed trait FeatureItem {
-  val containsVariable: Boolean
-}
+sealed trait FeatureItem
 
 sealed trait FeatureValue extends FeatureItem
 
 case class FeatureStruct(features: Map[String, FeatureItem]) extends FeatureValue {
-
-  val containsVariable = features.exists(_._2.containsVariable)
 
   def apply(featName: String): Option[FeatureItem] = features.get(featName)
 
@@ -20,29 +16,27 @@ case class FeatureStruct(features: Map[String, FeatureItem]) extends FeatureValu
 
 }
 
-sealed trait FeatureSimpleValue extends FeatureItem
-
-case object FPlaceholder extends FeatureSimpleValue {
-  override val containsVariable: Boolean = false
+case object FPlaceholder extends FeatureItem {
 
   override def toString: String = "_"
+
 }
 
-case class FVariable(name: String) extends FeatureSimpleValue {
-  override val containsVariable: Boolean = true
+case class FVariable(name: String) extends FeatureItem {
 
   override def toString: String = s"?$name"
+
 }
 
-case class FConst(value: String) extends FeatureValue with FeatureSimpleValue {
-  override val containsVariable: Boolean = false
+case class FConst(value: String) extends FeatureValue {
 
   override def toString: String = value
+
 }
 case class FList(elements: List[FeatureItem]) extends FeatureValue {
-  override val containsVariable: Boolean = elements.exists(_.containsVariable)
 
   override def toString: String = elements.mkString("<", ",", ">")
+
 }
 
 object FeatureStruct {
