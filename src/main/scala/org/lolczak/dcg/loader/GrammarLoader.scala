54768ea725a97e5ff2237acb5c63f7aa59587a1b
lolczak <lukasz.jan.olczak@gmail.com>
2015-10-21 17:03:01 +0200

    better loader

--- src/main/scala/org/lolczak/dcg/loader/GrammarLoader.scala
+++ src/main/scala/org/lolczak/dcg/loader/GrammarLoader.scala
@@ -1,21 +1,39 @@
 package org.lolczak.dcg.loader
 
+import java.io.File
+
 import org.lolczak.dcg.model._
 import org.lolczak.dcg.parser.grammar.GrammarParser
 import org.lolczak.dcg.parser.grammar.GrammarParser._
 import org.lolczak.dcg.parser.grammar.ast._
 import org.lolczak.dcg.parser.language.guard.{GroovyGuardEval, GuardEval}
 
-import scalaz.{\/, -\/, \/-}
-//import scalaz._
+import scala.io.Source
 import scalaz.Scalaz._
+import scalaz.{-\/, \/, \/-}
 
 object GrammarLoader {
+
+  def load(grammarFile: File): GrammarFailure \/ Grammar =
+    for {
+      grammarTxt <- loadGrammar(grammarFile)
+      loader     =  buildResourceLoader(grammarFile)
+      grammar    <- load(grammarTxt, loader)
+    } yield grammar
+
+  private def buildResourceLoader(grammarFile: File): ResourceLoader =
+    new FileSystemLoader(List(grammarFile.getParent)) + classpathLoader
+
+  private def loadGrammar(grammarFile: File): ResourceLoadFailure \/ String = {
+    if (!grammarFile.exists()) -\/(ResourceLoadFailure(s"$grammarFile doesn't exist"))
+    else if (grammarFile.isDirectory) -\/(ResourceLoadFailure(s"$grammarFile is directory"))
+    else \/-(Source.fromFile(grammarFile, "UTF-8").mkString)
+  }
 
   def load(grammarTxt: String, resourceLoader: ResourceLoader = classpathLoader): GrammarFailure \/ Grammar = {
     for {
       grammarAst       <- parse(grammarTxt)
-      importDirectives = grammarAst.directives.collect { case ImportDirective(file) => file }//todo remove filter(_.isInstanceOf[ImportDirective]).map(_.asInstanceOf[ImportDirective].file)
+      importDirectives = grammarAst.directives.collect { case ImportDirective(file) => file }
       imports          <- loadImports(importDirectives, resourceLoader)
       guard            = new GroovyGuardEval(imports)
       lexicon          = Lexicon.fromProductions(grammarAst.terminals: _*)
@@ -24,7 +42,7 @@ object GrammarLoader {
     } yield Grammar(nonterminals, lexicon)
   }
 
-  def loadImports(importDirectives: List[String], resourceLoader: ResourceLoader): CodeLoadFailure \/ List[String] = {
+  private def loadImports(importDirectives: List[String], resourceLoader: ResourceLoader): CodeLoadFailure \/ List[String] = {
     val imports = importDirectives.map { case path =>
       resourceLoader.loadResource(path).toRightDisjunction(path)
     }
@@ -32,10 +50,10 @@ object GrammarLoader {
     if (errors.isEmpty)
       \/-(contents.collect { case \/-(content) => content})
     else
-      -\/(CodeLoadFailure( "Cannot load " + errors.collect { case -\/(path) => path} mkString(",")))
+      -\/(CodeLoadFailure("Cannot load " + errors.collect { case -\/(path) => path} mkString ","))
   }
 
-  def processNonterminals(nonterminals: List[AstProduction], guard: GuardEval): List[Production] = {
+  private def processNonterminals(nonterminals: List[AstProduction], guard: GuardEval): List[Production] = {
     nonterminals.flatMap { p =>
       val allRhs = permutate(p.rhs)
       val lhs = Term(p.lhs.name, p.lhs.fStruct)
@@ -43,7 +61,7 @@ object GrammarLoader {
     }
   }
 
-  def permutate(rhs: List[RhsSymbol]): List[List[Term]] = {
+  private def permutate(rhs: List[RhsSymbol]): List[List[Term]] = {
     rhs.foldLeft(List(List.empty[Term])) {
       case (acc, AstTerm(name, struct)) =>
         acc.map(list => list :+ Term(name, struct))
@@ -55,7 +73,7 @@ object GrammarLoader {
     }
   }
 
-  def parse(grammarTxt: String): ParsingFailure \/ GrammarAst = {
+  private def parse(grammarTxt: String): ParsingFailure \/ GrammarAst = {
     GrammarParser.parseGrammar(grammarTxt) match {
       case Success(grammarAst, _) => \/-(grammarAst)
       case fault: NoSuccess       => -\/(ParsingFailure(fault.toString))
@@ -66,4 +84,4 @@ object GrammarLoader {
 sealed trait GrammarFailure
 case class ParsingFailure(msg: String) extends GrammarFailure
 case class CodeLoadFailure(msg: String) extends GrammarFailure
-//case class ResourceLoadFailure(msg: String) extends GrammarFailure
+case class ResourceLoadFailure(msg: String) extends GrammarFailure
